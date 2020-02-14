0ab56cef5f31d6050b05cf9cebc9ec30f8096097
webferrol <xurxo@webferrol.com>
2020-02-14 09:17:16 +0100

    modificación experimental -utilizado createElement

--- js/fichero.js
+++ js/fichero.js
@@ -1,4 +1,19 @@
 document.querySelector("#precios").value = document.querySelector("#pizzas").options[0].value;
+
+const crearElemento=(texto)=>{
+    let elemento = document.createElement("div");
+    elemento.className="engadido";
+    let elTexto = document.createTextNode(texto);
+    elemento.appendChild(elTexto);
+    document.querySelector(".selector").appendChild(elemento);
+}
 document.querySelector("#pizzas").addEventListener("change",e=>{
     document.querySelector("#precios").value = e.target.options[e.target.selectedIndex].value;
+    crearElemento(e.target.options[e.target.selectedIndex].text);
+});
+
+document.addEventListener("click",e=>{
+    if(e.target.className==="engadido")
+        e.target.parentNode.removeChild(e.target);
+
 });
